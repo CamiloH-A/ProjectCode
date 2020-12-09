d726cfe7a67d22aaaf39e9ab6e50f7ddf0f063a8
CamiloH-A <73556562+CamiloH-A@users.noreply.github.com>
2020-12-09 15:43:59 -0500

    Add files via upload

--- js/popups.js
+++ js/popups.js
@@ -1,18 +1,19 @@
 var btnAbrirPopup = document.getElementById('btn__login'),
-    btnAbrirPopupR = document.getElementById('btn__r'),
     overlay = document.getElementById('overlay__login'),
-    overlay = document.getElementById('overlay__register'),
     popup = document.getElementById('popup'),
-    popup2 = document.getElementById('popup2'),
     btn__cerrar__popup = document.getElementById('btn__cerrar__popup'),
     btn__submit__login = document.getElementById('btn__submit__login'),
+    btnAbrirPopupR = document.getElementById('btn__r'),
+    overlay = document.getElementById('overlay__register'),
+    popup2 = document.getElementById('popup2'),
     btn__submit__admin = document.getElementById('btn__submit__admin'),
     btn__submit__ureg = document.getElementById('btn__submit__ureg'),
     btn__submit__uesp = document.getElementById('btn__submit__uesp'),
-    btn__forgot__pass = document.getElementById('forgot__pass'),
-    overlay__pass = document.getElementById('overlay__forgot__pass'),
+    BtnAbrirPopupPass = document.getElementById('forgot__pass'),
+    overlay__pass = document.getElementById('overlay__pass'),
     popup__pass = document.getElementById('popup__pass'),
-    btn__aceptar = document.getElementById('btn__aceptar__pass');
+    BtnCerrarPopupPass = document.getElementById('BtnCerrarPopupPass'),
+    BtnSubmitPass = document.getElementById('BtnSubmitPass');
 
 btnAbrirPopup.addEventListener('click', function() {
     overlay__login.classList.add('active');
@@ -34,8 +35,19 @@ btn__cerrar__popup2.addEventListener('click', function() {
     popup2.classList.remove('active');
 });
 
-
-btn__forgot__pass.addEventListener('click', function() {
+BtnAbrirPopupPass.addEventListener('click', function() {
+    overlay__login.classList.remove('active');
+    popup.classList.remove('active');
     overlay__pass.classList.add('active');
     popup__pass.classList.add('active');
+});
+
+BtnCerrarPopupPass.addEventListener('click', function() {
+    overlay__pass.classList.remove('active');
+    popup__pass.classList.remove('active');
+});
+
+BtnSubmitPass.addEventListener('click', function() {
+    overlay__pass.classList.remove('active');
+    popup__pass.classList.remove('active');
 });
